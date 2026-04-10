36d68e53134ae9ab6a11d3ffa6c7605ea14966ed
patelhetanshim <patelhetanshi7@gmail.com>
2026-04-10 22:23:29 +0530

    Update p1.java

--- p1.java
+++ p1.java
@@ -1,24 +1,38 @@
+import java.util.Scanner;
 
-    import java.util.Scanner;
+class p1 {
+    public static void main(String args[]) {
 
-public class p1 {
-    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int amount;
+
         System.out.print("Enter amount: ");
         amount = sc.nextInt();
 
-        int notes[] = {100, 50, 10, 5, 2, 1};
+        int n100, n50, n10, n5, n2, n1;
 
-        for (int i = 0; i < notes.length; i++) {
-            int count = amount / notes[i];
-            if (count > 0) {
-                System.out.println(notes[i] + " : " + count);
-                amount = amount % notes[i];
-            }
-        }
+        n100 = amount / 100;
+        amount = amount % 100;
+
+        n50 = amount / 50;
+        amount = amount % 50;
+
+        n10 = amount / 10;
+        amount = amount % 10;
+
+        n5 = amount / 5;
+        amount = amount % 5;
+
+        n2 = amount / 2;
+        amount = amount % 2;
+
+        n1 = amount;
+
+        System.out.println("100 Notes = " + n100);
+        System.out.println("50 Notes = " + n50);
+        System.out.println("10 Notes = " + n10);
+        System.out.println("5 Notes = " + n5);
+        System.out.println("2 Notes = " + n2);
+        System.out.println("1 Notes = " + n1);
     }
 }
-    
-
