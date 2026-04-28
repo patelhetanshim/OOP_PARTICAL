6bdf352e1221ecdbb7fe311fbba645d992d18a77
patelhetanshim <patelhetanshi7@gmail.com>
2026-04-28 23:51:00 +0530

    Update BillGenerator.java

--- BillGenerator.java
+++ BillGenerator.java
@@ -1,3 +1,12 @@
+/*
+Write a program for billing system for a shopping mall. Create a class BillGenerator that uses method 
+overloading to generate bills based on customer type:  
+generateBill(int itemTotal): For regular customers, apply no discount.  
+generateBill(int itemTotal, int discount): For privileged customers, apply flat discount in rupees.  
+generateBill(int itemTotal, double discountPercent): For festive offers, apply percentage discount.  
+Write a program to display the final bill amount using appropriate overloaded method based on 
+customer category
+*/
 import java.util.Scanner;
 
 class BillGenerator {
